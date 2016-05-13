47398c16184db8a5a74ce4fae03cf1d8e8a6e14c
Horácio Dias <horacio.dias92@gmail.com>
2016-05-13 13:07:27 -0300

    Criando dashboard e aplicando decorator login_required

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^entrar/$', 'django.contrib.auth.views.login', 
-        { 'template_name' : 'accounts/login.html' }, name='login'),  
+    url(r'^$', 'simplemooc.accounts.views.dashboard', name='dashboard'),
+    url(r'^entrar/$', 'django.contrib.auth.views.login',
+        { 'template_name' : 'accounts/login.html' }, name='login'),
     url(r'^sair/$', 'django.contrib.auth.views.logout', 
-        { 'next_page' : 'core:home' }, name='logout'),  
-    url(r'^cadastre-se/$', 'simplemooc.accounts.views.register', name='register'),  
+        { 'next_page' : 'core:home' }, name='logout'),
+    url(r'^cadastre-se/$', 'simplemooc.accounts.views.register', name='register'),
 )
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 from .forms import RegisterForm
+
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
 
 def register(request):
     template_name = 'accounts/register.html'
