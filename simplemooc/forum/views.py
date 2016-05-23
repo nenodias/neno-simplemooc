4cb751e18cba644abe2d4165bde2a68f0c27af75
Horácio Dias <horacio.dias92@gmail.com>
2016-05-23 00:01:45 -0300

    Exemplo de classview com View

--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView, View, ListView
 
-class ForumView(TemplateView):
+class ForumView(View):
 
-    template_name = 'forum/index.html'
+    def get(self, request, *args, **kwargs):
+        return render(request, 'forum/index.html')
 
 
 index = ForumView.as_view()
