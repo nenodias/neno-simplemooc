dc47e4d21cb4a894b65d5aed93219f9089cfe37d
Horácio Dias <horacio.dias92@gmail.com>
2016-05-23 00:33:01 -0300

    Atualizando e iniciando a app de forum

--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, View, ListView
 
-class ForumView(View):
+from .models import Thread, Reply
 
-    def get(self, request, *args, **kwargs):
-        return render(request, 'forum/index.html')
+class ForumView(ListView):
 
+    model = Thread
+    paginate_by = 2
+    template_name = 'forum/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ForumView, self).get_context_data(**kwargs)
+        context['tags'] = Thread.tags.all()
+        return context
 
 index = ForumView.as_view()
