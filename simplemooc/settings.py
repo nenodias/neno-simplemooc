0ee061887b7e602448741bc0f34f6f3483141d27
Horácio Dias <horacio.dias@yahoo.com>
2017-01-17 20:57:41 -0200

    Atualizando

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -98,6 +98,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
